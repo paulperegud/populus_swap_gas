13f6c5ac02e66996d63d74e10f415a2de3994a43
Paul Peregud <paulperegud@gmail.com>
2017-04-21 13:35:54 +0200

    solidity 0.4.10 bug

---
+++ contracts/LibContract.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.10;
+
+contract LibContract {
+    uint256 public constant varA = 2;
+    uint256 public constant varB = varA;
+    function LibContract() {
+    }
+
+}
---
+++ contracts/MainContract.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.10;
+
+import "./LibContract.sol";
+
+contract MainContract {
+    function MainContract() {
+    }
+}
