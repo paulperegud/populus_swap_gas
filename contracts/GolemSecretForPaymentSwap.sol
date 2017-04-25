bceab80bcfe60dd64b3092961a289b8825f8eebf
Paul Peregud <paulperegud@gmail.com>
2017-04-25 13:36:39 +0200

    cleanup

--- contracts/GolemSecretForPaymentSwap.sol
+++ contracts/GolemSecretForPaymentSwap.sol
@@ -3,58 +3,32 @@ pragma solidity ^0.4.10;
 import "./GolemNetworkToken.sol";
 
 contract GolemSecretForPaymentSwap {
-    address public sendr;
     address public requestor;
     address public provider;
 
-    address public recovered;
-
-    bytes32 public b0;
-    bytes32 public b1;
-    bytes32 public b2;
-    bytes32 public b3;
-
-    GolemNetworkToken gnt;
+    GolemNetworkToken public gnt;
 
     event MyDebug(bytes32 myvalue);
 
-    function GolemSecretForPaymentSwap(address _requestor, address _provider) {
-        sendr = msg.sender;
-        requestor = _requestor;
+    function GolemSecretForPaymentSwap(address _provider, address _gnt) {
+        gnt = GolemNetworkToken(_gnt);
+        requestor = msg.sender;
         provider = _provider;
     }
 
-    /* function gls() external returns (bytes32) { */
-    /*     return debugslot; */
-    /* } */
-
-    function enc(bytes32 bts) external returns (bytes32) {
-        return bts;
-    }
-
-    function sha3(bytes32 bts) external returns (bytes32) {
-        return sha3(bts);
-    }
-    
     function finalize(bytes32 secret, uint _value, bytes32 r, bytes32 s, uint8 v)
-        external returns (address) {
-
+        external {
 
         // Only the provider can resolve this. I think it is cheaper than including
         // the provider address in the signed message. Right?
         if (msg.sender != provider)
             throw;
 
-        b0 = secret;
         var sh = sha3(secret);
-        b1 = sh;
-        b2 = bytes32(_value);
         
         var h = sha3(sh, bytes32(_value));
-        b3 = h;
 
         var recoveredAddr = ecrecover(h, v, r, s);
-        recovered = recoveredAddr;
         if (recoveredAddr != requestor) {
             throw;
         }
@@ -69,14 +43,12 @@ contract GolemSecretForPaymentSwap {
 
         var deposit = gnt.balanceOf(this);
 
-        return requestor;
-
-
         var value = _value;
         if (value > deposit)
             value = deposit;  // The provider was cheated, send as much as possible.
             
         var rem = deposit - value;
+
         if (!gnt.transfer(provider, value))
             throw;
             
--- tests/test_swap.py
+++ tests/test_swap.py
@@ -10,15 +10,10 @@ def ops():
     pass
 
 def fund_and_finalize(chain, gnt, x):
-    # print("block: {}".format(chain.web3.eth.blockNumber))
     for i, addr in enumerate(tester.accounts[:10]):
         v = random.randrange(15000 * utils.denoms.ether, 82000 * utils.denoms.ether)
-        # print("i: {}, addr: {}, x: {}".format(i, encode_hex(addr), x))
         chain.wait.for_receipt(
             gnt.transact({'value': v, 'from': encode_hex(addr)}).create())
-        # print("block: {}".format(chain.web3.eth.blockNumber))
-    # print("block: {}".format(chain.web3.eth.blockNumber))
-    # print("finalize call")
     chain.wait.for_receipt(
         gnt.transact().finalize())
     assert not gnt.call().funding()
@@ -32,16 +27,19 @@ def test_swap(chain):
     p_addr = tester.accounts[2]
     bn = chain.web3.eth.blockNumber
     start, finish = bn+2, bn+11
-    # print("start: {}, finish: {}".format(start, finish))
     args = [r_addr, r_addr, start, finish]
     gnt, _ = chain.provider.get_or_deploy_contract('GolemNetworkToken', deploy_args=args)
     fund_and_finalize(chain, gnt, chain.web3.eth.coinbase)
-    args = [r_addr, p_addr]
-    swap, _ = chain.provider.get_or_deploy_contract('GolemSecretForPaymentSwap',
-                                                    deploy_transaction={
-                                                        'from': r_addr
-                                                    },
-                                                    deploy_args=args)
+    args = [p_addr, gnt.address]
+    swap, tx = chain.provider.get_or_deploy_contract('GolemSecretForPaymentSwap',
+                                                     deploy_transaction={
+                                                         'from': r_addr
+                                                     },
+                                                     deploy_args=args)
+    gas = chain.wait.for_receipt(tx)
+    print("contract deployment cost: {}".format(gas['gasUsed']))
+    chain.wait.for_receipt(
+        gnt.transact({'from': r_addr}).transfer(swap.address, 100000))
     random.seed(0)
 
     def mb(x):
@@ -54,13 +52,6 @@ def test_swap(chain):
     def tobyteslist(n, bts):
         return [ bts >> i & 0xff for i in reversed(range(0, n*8, 8)) ]
 
-    def u2bl(ustr):
-        r = []
-        for x in ustr:
-            n = mb(ord(x))
-            r.extend(list(reversed(tobyteslist(n, ord(x)))))
-        return r
-
     def cpack(n, bts):
         """Packs int into bytesXX"""
         import struct
@@ -68,35 +59,18 @@ def test_swap(chain):
         return struct.pack(fmt, *tobyteslist(n, bts))
 
     def charpack(n, chars):
+        """Use on bytes32 values returned by EVM"""
         import struct
         fmt = "!{}B".format(n)
         return struct.pack(fmt, *[ ord(c) for c in chars ])
 
     kdf_seed = random.getrandbits(32*8)
     i = random.randint(1, 100)
-
-    kdf_seed = "12345678901234567890123456789012"
-
-    kdf_packed = unicode(kdf_seed)
-    called = swap.call().enc(kdf_packed)
-    assert bytes(kdf_packed) == bytes(called)
-
-    called = swap.call().sha3(kdf_packed)
-    assert bytes(utils.sha3(kdf_packed)) == bytes(charpack(32, called))
-
-    kdf_seed = random.getrandbits(32*8)
-
-    kdf_packed = bytes(cpack(32, kdf_seed))
-    assert 32 == len(kdf_packed)
-    called = swap.call().enc(kdf_packed)
-    assert len(kdf_packed) == len(called)
-    assert kdf_packed == bytes(charpack(32, called))
-
     # secret represents partial evaluation of KDF derivation function
     # where KDF(kdf_seed, i) = sha3(kdf_seed ++ i)
     secret = cpack(30, kdf_seed) + cpack(2, i)
     assert len(secret) == 32
-    max_value = gnt.call().balanceOf(tester.accounts[9])
+    max_value = gnt.call().balanceOf(swap.address)
     value = random.randint(1, max_value)
     # in Solidity: sha3(sha3(secret), bytes32(_value)):
     msghash = utils.sha3(utils.sha3(secret) + cpack(32, value))
@@ -104,24 +78,11 @@ def test_swap(chain):
     (V, R, S) = sign_eth(msghash, r_priv)
     ER = cpack(32, R)
     ES = cpack(32, S)
+    assert gnt.address == swap.call().gnt()
     fin_txn_hash = swap.transact({"from": p_addr}).finalize(secret, value, ER, ES, V)
     txn = chain.wait.for_receipt(fin_txn_hash)
-    b0 = swap.call().b0()
-    assert charpack(32, b0) == secret;
-    sh = swap.call().b1()
-    assert charpack(32, sh) == utils.sha3(secret);
-    bytesvalue = swap.call().b2()
-    assert charpack(32, bytesvalue) == cpack(32, value)
-
-    req = swap.call().requestor()
-    assert req == '0x'+encode_hex(r_addr)
-
-    sender = swap.call().sendr()
-    assert sender == '0x'+encode_hex(r_addr)
-
-    recov = swap.call().recovered()
-    assert recov == req
-    assert recov == '0x'+encode_hex(r_addr)
+    print("finalize call costs: {}".format(txn['gasUsed']))
+    assert False
 
 
 def on_MyDebug(*args, **kwargs):
@@ -138,27 +99,3 @@ def sign_eth(rawhash, priv):
     r = utils.big_endian_to_int(signature[0:32])
     s = utils.big_endian_to_int(signature[32:64])
     return (v, r, s)
-
-# def sign_btc(msghash, priv, pub):
-#     V, R, S = bitcoin.ecdsa_raw_sign(msghash, priv)
-#     assert bitcoin.ecdsa_raw_verify(msghash, (V, R, S), pub)
-#     Q = bitcoin.ecdsa_raw_recover(msghash, (V, R, S))
-#     assert addr == bitcoin.encode_pubkey(Q, 'hex_compressed') if V >= 31 else bitcoin.encode_pubkey(Q, 'hex')
-#     return (V, R, S)
-
-# def test_greeter(chain):
-#     storer, _ = chain.provider.get_or_deploy_contract('Storer')
-#     greeter, _ = chain.provider.get_or_deploy_contract('Greeter')
-#     print("greeter: {}".format(greeter))
-#     zero = '0x0000000000000000000000000000000000000000'
-#     assert zero == greeter.call().getStorer()
-#     assert not greeter.call().checkStorer()
-#     set_txn_hash = greeter.transact().setStorer(storer.address)
-#     txn = chain.wait.for_receipt(set_txn_hash)
-#     print("txn: {}".format(txn['gasUsed']))
-#     assert False
-#     assert zero != greeter.call().getStorer()
-#     assert greeter.call().checkStorer()
-
-#     greeting = greeter.call().greet()
-#     assert greeting == 501
